414ad4bf10d51ad51a59e0b3e052cf2f525d1fa6
amoghubhalkar19 <118107744+amoghubhalkar19@users.noreply.github.com>
2024-05-11 02:37:08 +0530

    changing index.html for current input

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-deploy_response = requests.post(f'https://api.render.com/v1/services/{service_id}/deploys', headers=headers, files=files)
-# deploy_id = deploy_response.json()['id']
-# print(f'Deployment initiated with ID: {deploy_id}')
