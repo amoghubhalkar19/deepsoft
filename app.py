91a6a72536492730409832c116e9cea1749d4dcd
amoghubhalkar19 <118107744+amoghubhalkar19@users.noreply.github.com>
2024-05-11 14:41:16 +0530

    changing index.html for current input

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import time
 def main():
     st.title('Website Builder')
     user_text = st.text_input("Enter text:", "")
-    
+
     if user_text:
         st.write("You entered:", user_text)
-        code=utils.generate_code(user_text)
+        code = utils.generate_code(user_text)
         utils.write_to_file(code)
         utils.git_commit_push(r"D:\deepsoft", "changing index.html for current input")
         with st.spinner('Deploying website...'):
@@ -19,6 +19,14 @@ def main():
         st.write(f'Website Link: {website_url}')
         st.write("Here's your website:")
         comps.iframe(website_url, height=500, scrolling=True)
+
+        if st.button("Modifications"):
+            # Redirect to a new tab for modifications
+            st.write("""
+                <script>
+                    window.open('/modify', '_blank');
+                </script>
+            """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
