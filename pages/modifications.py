341b1050c0ab22cba54f51cb2088dd8480d088b0
amoghubhalkar19 <118107744+amoghubhalkar19@users.noreply.github.com>
2024-05-11 20:12:06 +0530

    changing index.html for current input

--- pages/modifications.py
+++ pages/modifications.py
@@ -8,10 +8,12 @@ st.write("Perform modifications here...")
 # Add modification functionalities here
 modified_text = st.text_input("Modify your website:", "")
 # You can add more prompts for other modifications
-
+html_content = ""
+with open("public/index.html", "r") as file:
+    html_content = file.read()
 # Generate and deploy modified website
 if st.button("Deploy Modified Website"):
-    code = utils.perform_modifications(modified_text)
+    code = utils.perform_modifications(modified_text, html_content)
     utils.write_to_file(code)
     utils.git_commit_push(r"D:\deepsoft", "changing index.html for modified input")
     with st.spinner('Deploying modified website...'):
