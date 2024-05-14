ed2c550764183e971bd91d9f6b03a9c005b30717
amoghubhalkar19 <118107744+amoghubhalkar19@users.noreply.github.com>
2024-05-15 01:35:55 +0530

    changing index.html for current input

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def main():
             utils.git_commit_push(r"D:\deepsoft", "changing index.html for current input") 
             with st.spinner('Deploying website...'):
                 website_url = utils.deploy_render_api()
-                time.sleep(90)
+                time.sleep(100)
             # Show website URL and iframe once deployment is complete
             st.write(f'Website Link: {website_url}')
             st.write("Here's your website:")
@@ -32,7 +32,7 @@ def main():
                 utils.git_commit_push(r"D:\deepsoft", "changing index.html for current input")
                 with st.spinner('Deploying website...'):
                     website_url = utils.deploy_render_api()
-                    time.sleep(90)
+                    time.sleep(100)
             # Show website URL and iframe once deployment is complete
                 st.write(f'Website Link: {website_url}')
                 st.write("Here's your website:")
